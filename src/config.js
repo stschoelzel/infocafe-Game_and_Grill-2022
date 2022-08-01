9ed11f52800164f72eafd4984b2e68cb180cc1f7
stschoelzel <info@stschoelzel.de>
2022-08-01 19:10:37 +0200

    auf woche 1 beschränkt

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 //      Woche 2 - 4 Rätsel ...
 const sa2020 = { name: "sa2020", chapters: [4, 5, 2, 9] };
 const sa2021 = { name: "sa2021", chapters: [5, 5, 5, 5, 3] };
-const sa2022 = { name: "sa2021", chapters: [14, 5, 5, 5] };
+const sa2022 = { name: "sa2021", chapters: [15] };
 
 // Freigeschaltete Woche
 export const unlocked = new Date().getDate() > 2 ? 5 : 3;
