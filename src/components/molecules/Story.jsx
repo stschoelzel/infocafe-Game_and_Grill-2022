1c8889cccab96baeb0d37f1e27299ee6247bb40d
Kevin <fraesh93@gmail.com>
2022-08-09 13:07:22 +0200

    adjustzed console style

--- src/components/molecules/Story.jsx
+++ src/components/molecules/Story.jsx
@@ -9,32 +9,31 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   background: black;
-box-shadow: 0px 0px 5px 3px rgba(0,0,0,1);
-   font-family: 'Source Code Pro', monospace;
-    color:lime;
-  font-weight:normal;
-
+  box-shadow: 0px 0px 2px 3px rgba(0, 0, 0, 1);
+  font-family: "Source Code Pro", monospace;
+  color: lime;
+  font-weight: normal;
 `;
 
-const Border =  styled.div`
-  padding:1rem;
+const Border = styled.div`
+  padding: 1rem;
   margin: 2rem 0;
-  border-radius:.5rem;
-  background:#bcbda9;
-  box-shadow: inset 3px 3px 2px 0px rgba(255,255,255,.6),inset -3px -3px 2px 0px rgba(0,0,0,.3), 0px 2px 10px 0px rgba(0,0,0,.5),0px 5px 10px 0px rgba(0,0,0,.5);
+  width: 100%;
+  border-radius: 0.5rem;
+  background: #bcbda9;
+  box-shadow: inset 2px 2px 1px 0px rgba(255, 255, 255, 0.6),
+    inset -2px -2px 2px 0px rgba(0, 0, 0, 0.6),
+    0px 2px 10px 0px rgba(0, 0, 0, 0.5), 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
 `;
-
 
 export const Story = ({ children, style }) => {
   return (
     <Border>
-
-    <Container style={style}>
-
-      {/* <Corner src="/img/screw.png" />
+      <Container style={style}>
+        {/* <Corner src="/img/screw.png" />
       <Corner2 /> */}
-      {children} <div id="cursor"></div>
-    </Container>
-      </Border>
+        {children} <div id="cursor"></div>
+      </Container>
+    </Border>
   );
 };
