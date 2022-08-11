17504e9d7913409df6c85fd7256e68c8127ab419
stschoelzel <info@stschoelzel.de>
2022-08-11 12:48:33 +0200

    UnlockChapterTemplate,  + new "InRiddle" Variante

--- src/components/templates/UnlockChapterTemplate.jsx
+++ src/components/templates/UnlockChapterTemplate.jsx
@@ -5,32 +5,27 @@ import { H1 } from "../atoms/H1";
 import { Story } from "../molecules/Story";
 import { H4 } from "../atoms/H4";
 import { StoreContext } from "../../helper/store";
-import { config } from "../../config";
 
 export const UnlockChapterTemplate = ({ chapter, date }) => {
   const { solveChapter } = React.useContext(StoreContext);
-  const day = date.split(".");
-  const { getRiddleState } = React.useContext(StoreContext);
 
   React.useEffect(() => {
-    if (new Date().getDate() >= day[0]) {
-      console.log(
-        getRiddleState({ chapter }, config.chapters[{ chapter } - 1])
-      );
-      solveChapter(chapter);
-    }
+    solveChapter(chapter);
   }, []);
-
   return (
-    <p>
-      Ihr habt die {chapter}. Woche des Abenteuers abgeschlossen! Gönnt euch
-      eine wohlverdiente Rast, bald geht es weiter.
-      <br />
-      <br />
-      Weiter geht es am:
-      <br />
-      <br />
-      <H4>{date}</H4>
-    </p>
+    <Parchment>
+      <H1>Glückwunsch!</H1>
+      <Story>
+        Ihr habt das {chapter}. Woche des Abenteuers abgeschlossen! Gönnt euch
+        eine wohlverdiente Rast, bald geht es weiter.
+        <br />
+        <br />
+        Das nächste Woche offenbart sich euch am
+        <br />
+        <br />
+        <H4>{date}</H4>
+        Kehrt dann zum letzten Ort eures Abenteuers zurück um fortzufahren!
+      </Story>
+    </Parchment>
   );
 };
